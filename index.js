b70bc84fd9d352ef03189bd5d2ce1a8d24f71bec
eric <ericmaina11455@gmail.com>
2024-03-20 12:10:16 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1=31
+const num2=2
+let multiply=num1*num2
+
+let random=Math.ceil(Math.random()*100)
+
+const num3=80
+const num4=20
+let mod=num3/num4
+
+const num5=3
+const num6=9
+const num7=20
+let max=Math.max(num5,num6,num7)
